22c8bb3afd5f79dbbe37fc1ed572e39823fcf047
Bilal <bilalbilin12@gmail.com>
2024-08-13 15:34:50 +0300

    Add project files.

---
+++ PatikaWeek5PracticeIMDBList/Movie.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek5PracticeIMDBList
+{
+    public class Movie // Creating a new class
+    {   
+        // Defining fields
+        private double _imdbRate; 
+
+        private string _movieName;
+
+        // Defining properties
+        public double ImdbRate {  get; set; }
+
+        public string MovieName { get; set; }
+
+        public Movie(double imdbRate, string movieName) // Building an constructor that gets parameters
+        {
+            ImdbRate = imdbRate;
+            MovieName = movieName;
+        }
+
+        public void DisplayAllMovies() // Method that display all movies
+        {
+            Console.WriteLine($"Film adı: {MovieName}, IMDB Puanı: {ImdbRate}");
+        }
+
+        public void DisplayOnly4Between9() // Method that display movies only imdb rate is between 4 and 9
+
+        {
+            if (ImdbRate <= 9 && ImdbRate >= 4) // Condition
+            {
+                Console.WriteLine($"Film adı: {MovieName}, IMDB Puanı: {ImdbRate}");
+            }
+        }
+
+        public void DisplayStartingLetterA() // Method that display movies only starting with letter "A"
+        {
+            
+            if (MovieName.ToLower().StartsWith("a")) // Condition
+            {
+                Console.WriteLine($"Film adı: {MovieName}, IMDB Puanı: {ImdbRate}");
+            }
+        }
+
+    }
+}
---
+++ PatikaWeek5PracticeIMDBList/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using PatikaWeek5PracticeIMDBList;
+
+// Creating an new object list
+List<Movie> movieList = new List<Movie>();
+
+while (true) // loop that will take data until the user finishes
+{
+    
+    Console.WriteLine("Lütfen girmek istediğiniz filmin adını yazınız(Bitirmek istediğinizde lütfen q yazınız): ");
+    string movieName = Console.ReadLine(); 
+
+    if (movieName.ToLower() == "q") // The condition to end loop
+    {
+        break;
+    }
+
+    Console.WriteLine("Lütfen filmin imdb puanını yazınız: ");
+    bool basarili = double.TryParse(Console.ReadLine(), out double imdbRate);
+
+    if (basarili && imdbRate >= 0 && imdbRate <= 10) // Checking the data whether it is double and between 4 and 9
+    {
+        Movie movie = new Movie(imdbRate, movieName);
+        movieList.Add(movie); // Adding movie object to the list 
+
+    }
+
+    else
+    {
+        Console.WriteLine("Hatalı giriş yaptınız! Lütfen 0 ile 10 arasında geçerli bir imdb puanı giriniz.");
+    }
+}
+
+Console.WriteLine("----- Tüm Filmler -----"); // Display all movie
+foreach (var movie in movieList) 
+{
+    movie.DisplayAllMovies();
+}
+
+Console.WriteLine("----- İmdb Puanı 4 ve 9 Arasındaki Filmler -----");
+foreach (var movie in movieList) // Display movies that imdb rate between 4 and 9
+{
+    movie.DisplayOnly4Between9();
+}
+
+Console.WriteLine("------- A Harfi ile Başlayan Filmler --------"); 
+foreach (var movie in movieList) // Display movies that start with letter "A"
+{
+    movie.DisplayStartingLetterA();
+}
